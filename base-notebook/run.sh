4c9b7577d65a00ced0aba8c2cb959a34a5a4c38b
lzfxxx <lzfxxx@gmail.com>
2018-07-09 23:55:45 +0800

    nvidia configs

--- base-notebook/run.sh
+++ base-notebook/run.sh
@@ -10,6 +10,6 @@ source /usr/local/bin/virtualenvwrapper.sh
 #lsvirtualenv
 #mktmpenv
 #toggleglobalsitepackages
-/home/jovyan/.virtualenvs/jlenv/bin/tensorboard --logdir=${WORK}/logs --host=0.0.0.0 &
+tensorboard --logdir=${WORK}/logs --host=0.0.0.0 &
 /home/jovyan/.virtualenvs/jlenv/bin/jupyter labhub --ip=0.0.0.0 --NotebookApp.allow_origin=* --NotebookApp.token='' --NotebookApp.notebook_dir=${WORK}
 #python3 -m http.server 8888
