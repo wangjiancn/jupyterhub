c13dc867f5a5e68fd0ba2a61ff2656db7b960c88
lzfxxx <lzfxxx@gmail.com>
2018-08-21 15:13:33 +0800

    Acrobaticat/mo#51 pyls backend

--- base-notebook/install_venv.sh
+++ base-notebook/install_venv.sh
@@ -12,7 +12,7 @@ if [ ! -e ${REQ_TXT}  ]  ; then
     exit
 fi
 
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python
+export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
 export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/bin/virtualenv
 source /usr/local/bin/virtualenvwrapper.sh
 
