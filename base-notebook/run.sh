936b4c75dc832411ac2e5a0ebcff460e6f8f1f7b
lzfxxx <lzfxxx@gmail.com>
2018-12-04 22:24:57 +0800

    default_url test

--- base-notebook/run.sh
+++ base-notebook/run.sh
@@ -24,4 +24,5 @@ workon .localenv
 
 tensorboard --logdir=${WORK}/results/tb_results --host=0.0.0.0 &
 cd /home/jovyan/pyls && npm run start:ext &
-${WORK}/.localenv/bin/jupyter labhub --ip=0.0.0.0 --NotebookApp.allow_origin=* --NotebookApp.iopub_data_rate_limit=10000000000 --NotebookApp.notebook_dir=${WORK}
+${WORK}/.localenv/bin/jupyter labhub --ip=0.0.0.0 --NotebookApp.allow_origin=* --NotebookApp.iopub_data_rate_limit=10000000000 \
+--NotebookApp.notebook_dir=${WORK} --NotebookApp.default_dir=/lab/tree/OVERVIEW.md
