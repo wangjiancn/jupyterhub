14823cc67e19f32109f49e54f2eab5e2b51b4d94
lzfxxx <lzfxxx@gmail.com>
2019-07-10 21:42:41 +0800

    rm test_dir

--- base-notebook/test_dir/test.py
+++
@@ -1,6 +0,0 @@
-print('job start')
-with open('/mnt/out_test.txt', 'w') as f:
-    f.write('write some thing')
-print('write finish')
-from subprocess import check_output
-print(check_output('pip list', shell=True))
