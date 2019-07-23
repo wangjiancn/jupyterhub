f55abb7071ce1807c63b15d19c26ecdc150722a8
lzfxxx <lzfxxx@gmail.com>
2019-07-23 12:15:41 +0800

    #2246 OVERVIEW.md -> _OVERVIEW.md README.ipynb -> _README.ipynb

--- base-notebook/run.sh
+++ base-notebook/run.sh
@@ -25,4 +25,4 @@ workon .localenv
 #tensorboard --logdir=${WORK}/results/tb_results --host=0.0.0.0 &
 cd /home/jovyan/pyls && npm run start:ext &
 ${WORK}/.localenv/bin/jupyter labhub --ip=0.0.0.0 --NotebookApp.allow_origin=* --NotebookApp.disable_check_xsrf=True --NotebookApp.iopub_data_rate_limit=10000000000 \
---NotebookApp.notebook_dir=${WORK} --NotebookApp.default_dir=/lab/tree/OVERVIEW.md
+--NotebookApp.notebook_dir=${WORK} --NotebookApp.default_dir=/lab/tree/_OVERVIEW.md
